0917289fdebebc999ec566fad8f768df7cbe8aa7
Madnessianin <madnessstalkerl@gmail.com>
2020-10-22 13:05:28 +0500

    Updates

---
+++ precache-manifest.f570b951caaaadd01845252ff8aca057.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8707aafcec6cf581b279068f67e3ade1",
+    "url": "/ReactSocialNetwork/index.html"
+  },
+  {
+    "revision": "2354762d250aea51cfc7",
+    "url": "/ReactSocialNetwork/static/css/3.df155852.chunk.css"
+  },
+  {
+    "revision": "2b59e7805ce3c448a3ba",
+    "url": "/ReactSocialNetwork/static/css/4.0157a892.chunk.css"
+  },
+  {
+    "revision": "41c261a755ee20d58db7",
+    "url": "/ReactSocialNetwork/static/css/main.ad4ed390.chunk.css"
+  },
+  {
+    "revision": "b0781f542a444e677561",
+    "url": "/ReactSocialNetwork/static/js/2.48d7b755.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/ReactSocialNetwork/static/js/2.48d7b755.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2354762d250aea51cfc7",
+    "url": "/ReactSocialNetwork/static/js/3.09393b3a.chunk.js"
+  },
+  {
+    "revision": "2b59e7805ce3c448a3ba",
+    "url": "/ReactSocialNetwork/static/js/4.e43c8821.chunk.js"
+  },
+  {
+    "revision": "41c261a755ee20d58db7",
+    "url": "/ReactSocialNetwork/static/js/main.9f4ecb06.chunk.js"
+  },
+  {
+    "revision": "797e806894cfbb8a2f8f",
+    "url": "/ReactSocialNetwork/static/js/runtime-main.9aefde0d.js"
+  },
+  {
+    "revision": "7a1293ee54bf42784c1edf32d0cb5378",
+    "url": "/ReactSocialNetwork/static/media/Preloader.7a1293ee.svg"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/ReactSocialNetwork/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "775b91d4b1bfcac2aa3292b47763c1b1",
+    "url": "/ReactSocialNetwork/static/media/user.775b91d4.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.855ef4ff618721c420abfeb1d67e584d.js"
+  "/ReactSocialNetwork/precache-manifest.f570b951caaaadd01845252ff8aca057.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/ReactSocialNetwork/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
