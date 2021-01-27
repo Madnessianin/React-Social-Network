a5eca69b868c8a1eb4d4361ad96525c168a473bb
Madnessianin <madnessstalkerl@gmail.com>
2021-01-27 14:18:50 +0500

    fix version axios

--- src/components/Common/FormsControl/FormsControl.jsx
+++ src/components/Common/FormsControl/FormsControl.jsx
@@ -1,15 +1,18 @@
 import React from 'react'
-import styles from './FormsControl.module.css'
+import style from './FormsControl.module.css'
 
 
 const FormControl = ({meta,input,child, ...props}) => {
     const hasError = meta.error && meta.touched;
     return (
-        <div className = {styles.formControl + " " + (hasError ? styles.error : "")}>
-            <div>
+        <div className = {style.formControl}>
+            <div className = {style.inputWrapper  + " " + (hasError ? style.error : "")}>
                 {props.children}
             </div>
-            {hasError && <span>{meta.error}</span>}
+            {hasError && <div className = {style.innerError}>
+                            <i className = "fas fa-exclamation" />
+                            <span className = {style.textError}>{meta.error}</span>
+                        </div>}
         </div>
     )
 }
@@ -22,4 +25,30 @@ export const Textarea = (props) => {
 export const Input = (props) => {
     const {meta,input,child, ...restProps} = props;
     return <FormControl {...props}><input {...input} {...restProps} /></FormControl>   
+}
+
+
+export const RadioGroup = ({meta,input, ...props}) => {
+    return (
+        <div className = {style.radioWrapper}>
+            <label className = {style.radioBtnValue}><input {...input} {...props} value = 'men' id = {'men'} checked />{'Men'}</label>
+            <label className = {style.radioBtnValue}><input {...input} {...props} value = 'women' id = {'women'} checked />{'Women'}</label>
+        </div>
+    )
+}
+
+export const Checkbox = ({meta,input, ...props}) => {
+    return (
+        <div className = {style.checkbox}>
+            <input {...input} {...props} />
+        </div>
+    )
+}
+
+export const MyDate = ({meta,input, ...props}) => {
+    return (
+        <div className = {style.date}>
+            <input {...input} {...props} />
+        </div>
+    )
 }
