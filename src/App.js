818840bd052586cce727f5ce1eaa0e0abe786e97
Madnessianin <madnessstalkerl@gmail.com>
2021-03-24 20:05:16 +0500

    fix login component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import React, { useEffect } from "react";
 import "./App.scss";
 import { Redirect, Route, Switch } from "react-router-dom";
-import LoginConteiner from "./Components/Login/LoginConteiner";
 import { initializeApp } from "./Redux/app-reducer";
 import { useDispatch, useSelector } from "react-redux";
 import Preloader from "./Components/Common/Preloader/Preloader";
 import { getInitialized } from "./Redux/app-selectors";
 import Page from "./Components/Page/Page";
+import Login from "./components/Login/Login";
 
 const App = () => {
   const dispach = useDispatch();
@@ -23,7 +23,7 @@ const App = () => {
     <div className="conteiner">
       <Switch>
         <Route exact path="/" render={() => <Redirect to={"/app"} />} />
-        <Route path="/login" render={() => <LoginConteiner />} />
+        <Route path="/login" render={() => <Login />} />
         <Route path="/app" render={() => <Page />} />
       </Switch>
     </div>
--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -1,74 +1,104 @@
 import React from "react";
 import { Redirect } from "react-router-dom";
 import { Field, reduxForm } from "redux-form";
-import { required } from "../../Utils/Validators/validators";
-import { Input } from "../Common/FormsControl/FormsControl";
-import style from "./Login.module.css";
+import style from "./Login.scss";
 import logo from "../../assets/images/logo192.png";
 import classes from "../Common/FormsControl/FormsControl.module.css";
-import Button from "../Common/Button/Button";
-const LoginForm = (props) => {
+import { useDispatch, useSelector } from "react-redux";
+import { login } from "../../Redux/auth-reducer";
+import { getCaptcha, getIsAuth } from "../../Redux/auth-selectors";
+import { Form, Input, Button, Checkbox } from 'antd';
+import { UserOutlined, LockOutlined } from '@ant-design/icons';
+import { styleBtn, styleBtnLink, styleCheckBox, styleInput } from "./LoginStyle";
+
+const LoginForm = ({onSubmit, captchaURL, error}) => {
   return (
-    <form className={style.formInner} onSubmit={props.handleSubmit}>
-      <div className={style.field}>
-        <Field
-          placeholder={"Login"}
-          name={"email"}
-          component={Input}
-          validate={[required]}
+    <Form
+      name="login"
+      className="login_form_inner"
+      initialValues={{ remember: true }}
+      onFinish={onSubmit}
+    >
+      <Form.Item
+        name="email"
+        rules={[{ required: true, message: 'Please input your Username!' }]}
+      >
+        <Input prefix={<UserOutlined className="site-form-item-icon" />} placeholder="Username" style={styleInput} />
+      </Form.Item>
+      <Form.Item
+        name="password"
+        rules={[{ required: true, message: 'Please input your Password!' }]}
+      >
+        <Input.Password 
+          prefix={<LockOutlined className="site-form-item-icon" />}
+          type="password"
+          placeholder="password"
+          style={styleInput}
         />
-      </div>
-      <div className={style.field}>
-        <Field
-          placeholder={"password"}
-          name={"password"}
-          component={Input}
-          type={"password"}
-          validate={[required]}
-        />
-      </div>
-      <div className={style.inner}>
-        <Button textBtn={"Sign in"} type={"submit"} />
-        <div className={style.checkbox}>
-          <Field
-            component={Input}
-            name={"rememberMe"}
-            id={"rememberMe"}
-            type={"checkbox"}
-          />
-          <label className={style.checkboxLabel} htmlFor={"rememberMe"}>
-            remember me
-          </label>
+      </Form.Item>
+      <Form.Item>
+        <div className="rememberMe">
+          <Form.Item name="rememberMe" valuePropName="checked" noStyle>
+            <Checkbox style={styleCheckBox}>Запомнить меня</Checkbox>
+          </Form.Item>
+
+          <Button type="link" style={styleBtnLink}>
+            Забыли пароль?
+          </Button>
         </div>
-      </div>
-      {props.captchaURL && (
-        <div>
-          <img className={style.captcha} src={props.captchaURL} />
-          <Field component={Input} name={"captcha"} />
+      </Form.Item>
+
+      <Form.Item>
+        <div className="login_btn">
+          <Button type="primary" htmlType="submit" style={styleBtn}>
+            Войти
+          </Button>
+          <Button style={styleBtn}>
+            Зарегистрироваться
+          </Button>
         </div>
+        
+      </Form.Item>
+      {captchaURL && (
+        <Form.Item 
+          name="captcha"
+          rules={[{ required: true, message: 'Please input your Username!' }]}>
+          <img className={style.captcha} src={captchaURL} />
+          <Input placeholder="captcha"
+                 style={styleInput} />
+        </Form.Item>
       )}
-      {props.error && (
-        <div className={classes.formCommonError}>{props.error}</div>
+      {error && (
+        <div className={classes.formCommonError}>{error}</div>
       )}
-    </form>
+    </Form>
+    
   );
 };
-const LoginFormConteiner = reduxForm({ form: "login" })(LoginForm);
 
-const Login = (props) => {
+//const LoginFormConteiner = reduxForm({ form: "login" })(LoginForm);
+
+const Login = () => {
+  const dispatch = useDispatch()
+
+  const isAuth = useSelector(state => getIsAuth(state))
+  const captchaURL = useSelector(state => getCaptcha(state))
+
   const onSubmit = (data) => {
-    props.login(data);
+    dispatch(login(data));
   };
 
-  if (props.isAuth) {
-    return <Redirect to={"/profile"} />;
+  if (isAuth) {
+    return <Redirect to={"app/profile"} />;
   }
 
   return (
-    <div className={style.form}>
-      <img className={style.logo} src={logo} />
-      <h1 className={style.formTitle}>Social network</h1>
-      <LoginFormConteiner onSubmit={onSubmit} captchaURL={props.captchaURL} />
+    <div className="login_form">
+      <img className="login_logo" src={logo} />
+        <div className = "login_wrapper">
+          <h3 className="login_title">Социальная сеть</h3>
+          <LoginForm onSubmit={onSubmit} captchaURL={captchaURL} />
+        </div>
     </div>
   );
 };
