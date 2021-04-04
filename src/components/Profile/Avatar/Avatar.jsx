1932eabeb41f597df2605c780a7457db0d60909d
Madnessianin <madnessstalkerl@gmail.com>
2021-04-04 20:17:08 +0500

    fix upload btn

--- src/components/Profile/Avatar/Avatar.jsx
+++ src/components/Profile/Avatar/Avatar.jsx
@@ -5,7 +5,7 @@ import userPhoto from "../../../assets/images/user.png";
 import { getProfilePhoto } from "../../../Redux/profile-selectors";
 import { Upload, Image, Button, message } from "antd";
 import { UploadOutlined } from "@ant-design/icons";
-import style from "./Avatar.module.scss"
+import style from "./Avatar.module.scss";
 import { Link } from "react-router-dom";
 
 const MyAvatar = ({ isOwner }) => {
@@ -21,41 +21,48 @@ const MyAvatar = ({ isOwner }) => {
 
 const EditBlock = () => {
   const dispatch = useDispatch();
-  
+
   const beforeUpload = (file) => {
-    if (file.type !== 'image/jpeg' || file.type !== 'image/jpg' || file.type !== 'image/png') {
-      message.error("Выбран некорректный файл!")
+    if (
+      file.type === "image/jpeg" ||
+      file.type === "image/jpg" ||
+      file.type === "image/png"
+    ) {
+      return true;
+    } else {
+      message.error("Выбран некорректный файл!");
+      return false;
     }
-  }
-  
-  const dispatchPhoto = ({file}) => {
+  };
+
+  const dispatchPhoto = ({ file }) => {
     dispatch(savePhoto(file));
   };
   return (
     <div className={style.btnBlock}>
       <Upload customRequest={dispatchPhoto} beforeUpload={beforeUpload}>
-        <Button
-          className={style.firstBtn}
-          icon={<UploadOutlined />}
-        >
+        <Button className={style.firstBtn} icon={<UploadOutlined />}>
           Загрузить фото
         </Button>
       </Upload>
       <Link to="/app/edit">
-        <Button className={style.lastBtn} type="primary">Редактировать</Button>
+        <Button className={style.lastBtn} type="primary">
+          Редактировать
+        </Button>
       </Link>
     </div>
-  )
+  );
 };
 
 const UserBlock = () => {
   return (
     <div className={style.btnBlock}>
       <Button className={style.firstBtn}>Написать сообщение</Button>
-      <Button className={style.lastBtn} type="primary">Добавить в друзья</Button>
+      <Button className={style.lastBtn} type="primary">
+        Добавить в друзья
+      </Button>
     </div>
-  )
-}
-
+  );
+};
 
 export default MyAvatar;
